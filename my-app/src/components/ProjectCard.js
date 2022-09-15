5af920865b04e20b4cd041d4c599169ef87e4c85
nawZeroX239 <naw122333444455555@gmail.com>
2022-09-14 22:44:47 -0700

    moved particle js component from hero to homepage

--- my-app/src/components/ProjectCard.js
+++ my-app/src/components/ProjectCard.js
@@ -1,9 +1,25 @@
 import React from "react"
 
+import projectPhoto from '../images/hash-table.png'
+
 export default function ProjectCard() {
   return (
     <div className="project-card">
-        <img></img>
+      <div className="project-figure">
+        <img
+          src={projectPhoto}
+          className="project-photo"
+          alt="project-photo"
+        />
+        <div className="project-tags">
+          <p>C</p>
+          <p>C++</p> 
+        </div>
+      </div>
+      <div className="project-info">
+        <h2 className="project-title">Query Processor</h2>
+      </div>
+        
     </div>
   )
 }
--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,9 +1,11 @@
 import React from "react"
 
+import ProjectCard from './ProjectCard'
 export default function Projects() {
   return (
     <div className="projects">
       <h1 className="projects-section-title"> Projects </h1>
+      <ProjectCard />
     </div>
   )
 }
