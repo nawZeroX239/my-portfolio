55d42ce072badc60f4aa0b25eb25bde913521b8f
nawZeroX239 <naw122333444455555@gmail.com>
2022-09-16 21:26:26 -0700

    reafactoring code to increase readibility

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -13,20 +13,27 @@ export default function Hero() {
     <div className="hero">
 
       <div className="hero-greeting">
-        <h3 className="greeting-small no-margin"> HELLO WORLD. <span className="greeting-large"> I am <span style={{color:"#FCA311"}}>Naw</span>. </span>  </h3> 
+        <h3 className="greeting-small no-margin"> HELLO WORLD.
+          <span className="greeting-large"> I am
+            <span style={{color:"#FCA311"}}> Naw </span>.
+          </span> 
+        </h3> 
       </div>
+
       <p className="hero-intro">
         I am in my third year computer science student @ UW Seattle.  I am actively looking a software internship for summer 2023.
       </p>
 
       <div className="hero-actions">
-        <button className="action-button" onClick={()=>redirect(resumeURL)}>Resume</button>
+        <button className="action-button" onClick={()=>redirect(resumeURL)}>
+          Resume
+        </button>
         <button className="action-button" onClick={()=>redirect(linkedInURL)}>
           LinkedIn
         </button>
         <button className="action-button" onClick={()=>redirect(githubURL)}>
           Github
-          </button> 
+        </button> 
       </div>
     </div>
   )
