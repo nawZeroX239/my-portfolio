8b7d17ba46728f0590e343202a2e2e23d484ca6f
nawZeroX239 <naw122333444455555@gmail.com>
2022-09-16 21:22:40 -0700

    redesign hero action buttons and now it correctlys redirect to external page

--- my-app/src/components/Academics.js
+++ my-app/src/components/Academics.js
@@ -4,7 +4,10 @@ export default function Academics() {
   return (
     <div className="academics" id="academics">
       <h1 className="section-title"> Academics </h1>
-      <p>academics course here</p>
+      <h3>Relevant courses</h3>
+      <div className="course-container">
+        
+      </div>
     </div>
   )
 }
--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -3,6 +3,12 @@ import React from "react"
 import "./Hero.css"
 
 export default function Hero() {
+  const linkedInURL = "https://www.linkedin.com/in/naw-naw-4548b81a4/";
+  const githubURL =  "https://github.com/nawZeroX239";
+  const resumeURL = "https://drive.google.com/file/d/1JR2L23ZGlIhsfhdS2AqJ6w7ebkyb4bDU/view?usp=sharing";
+  const redirect = (url) => {
+     window.location.assign(url);
+  }
   return (
     <div className="hero">
 
@@ -12,10 +18,15 @@ export default function Hero() {
       <p className="hero-intro">
         I am in my third year computer science student @ UW Seattle.  I am actively looking a software internship for summer 2023.
       </p>
+
       <div className="hero-actions">
-        <button className="actions-bttn">Resume</button>
-        <button className="actions-bttn">LinkedIn </button>
-        <button className="actions-bttn">Github</button> 
+        <button className="action-button" onClick={()=>redirect(resumeURL)}>Resume</button>
+        <button className="action-button" onClick={()=>redirect(linkedInURL)}>
+          LinkedIn
+        </button>
+        <button className="action-button" onClick={()=>redirect(githubURL)}>
+          Github
+          </button> 
       </div>
     </div>
   )
